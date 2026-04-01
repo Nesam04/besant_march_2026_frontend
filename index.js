682259a2585c260267ca7f2b0f823a4025f1167d
Dinesh107 <dineshjr107@gmail.com>
2026-04-01 19:37:59 +0530

    eigth commit js introduction

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log(`Welocme to js world`);
+// console.log("Welocme to js world");
+// console.log('Welocme to js world');
+
+
+// window.alert("Hai hello students");
+/*
+
+window.alert("I like food")
+
+*/
+
+document.getElementById("myH1").textContent = "Hello Students";
+
+document.getElementById("myP").textContent = "I like food";
